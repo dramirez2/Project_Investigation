c9a07d5b38672fbc9fce3afdada3c3c4ee3b0a13
Daniel Ramirez <dramirez2@users.noreply.github.com>
2014-11-06 20:14:23 -0400

    Removed info

--- investigaProfeCCOM.php
+++ investigaProfeCCOM.php
@@ -4,9 +4,9 @@
 //select inv_id from Investigacion where titulo="" and descripcion=""
 
 $host = "localhost";
-$usuario = "dramirez2";
-$passw = "turntablepower2";
-$estu_investigacion = "estu_investigacion";
+$usuario = "username";
+$passw = "password";
+$estu_investigacion = "database";
 
 $conexion = mysql_connect($host, $usuario, $passw);
 mysql_select_DB($estu_investigacion);
